1340f7a22cf3bae051ab3a87cef4ec896b2b51d1
denin9w <denin.9w@gmail.com>
2026-09-03 10:56:13 -0300

    atualização

--- style/matriculas/script/script.js
+++ style/matriculas/script/script.js
@@ -1,16 +1,27 @@
 
-let etapa1 = document.querySelector('.etapa1');
-let etapa2 = document.querySelector('.etapa2');
+const etapa1 = document.querySelector('.etapa1');
+const etapa2 = document.querySelector('.etapa2');
 
-let btnproximo = document.querySelector('#btnProximo');
-let btnvoltar = document.querySelector('#btnVoltar');
+const btnproximo = document.querySelector('#btnProximo');
+const btnvoltar = document.querySelector('#btnVoltar');
+const btnfinalizar = document.querySelector('#btnFinalizar');
+
+const formulario = document.getElementById('formulario');
 
 function proximo(){
     etapa1.style.display = 'none';
     etapa2.style.display = 'block';
+    btnproximo.style.display = 'none';
+    btnfinalizar.style.display = 'block';
 }
 
 function voltar(){
     etapa1.style.display = 'block';
     etapa2.style.display= 'none';
+    btnproximo.style.display = 'block';
+    btnfinalizar.style.display = 'none';
+}
+
+function finalizar(){
+    // Finalize the registration logic here
 }
